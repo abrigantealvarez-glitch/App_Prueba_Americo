70c6a9a4e0ddaa94cfea76ec1b37100bbbbe1f0d
abrigantealvarez-glitch <abrigantealvarez@gmail.com>
2025-08-25 13:16:26 -0500

    Update App_Prueba.py

--- App_Prueba.py
+++ App_Prueba.py
@@ -42,12 +42,10 @@ df_filtro = df[
 ]
 
 num_clientes = df_filtro["ClienteID"].nunique()
-num_transacciones = df_filtro["VentaID"].nunique()
 venta_total = df_filtro["Monto"].sum()
 
 # 5. Mostrar resultados
 st.metric("Clientes únicos", num_clientes)
-st.metric("Número de transacciones", num_transacciones)
 st.metric("Venta total oferta", venta_total)
 
 # Pareto
